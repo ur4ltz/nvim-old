715b63a00f00a8447625e616fdb3c805b5008ef1
Andy Shevchenko <ur4ltz@gmail.com>
2020-12-18 17:01:25 +0200

    Update configure.lua

--- lua/configure.lua
+++ lua/configure.lua
@@ -1,10 +1,10 @@
 local o   = vim.o
 local g   = vim.g
+local wo  = vim.wo
 local bo  = vim.bo
 local api = vim.api
 
-g.loaded_python_provider = 0
-
+-- Global options {{{
 o.updatetime    = 800
 o.termguicolors = true
 o.mouse         = 'n'           -- Enable mouse
@@ -14,29 +14,35 @@ o.wildmenu      = true          -- Command line completion mode
 o.wildmode      = 'full'        -- Command line completion mode
 o.hlsearch      = true
 
+o.listchars     = [[tab:→\ ,space:·,eol:¶,trail:·,extends:↷,precedes:↶]]
+
 o.clipboard     = 'unnamedplus' -- Use system clipboard
 
 o.backup        = true
-local backupdir = os.getenv('HOME') .. '/.vim/files/backup//'
+local backupdir = os.getenv('HOME') .. '/.vim/files/backup/'
 o.backupdir     = backupdir
 
 o.undofile      = true
 o.undolevels    = 10000
 o.undoreload    = 10000
-local undodir   = os.getenv('HOME') .. '/.vim/files/undo//'
+local undodir   = os.getenv('HOME') .. '/.vim/files/undo/'
 o.undodir       = undodir
+-- }}}
 
-api.nvim_exec([[
+-- Window options {{{
+wo.number       = true
+wo.list         = true
 
-   set number
+wo.cursorline   = true
+wo.cursorcolumn = true
+-- }}}
 
-   set cursorline
-   set cursorcolumn
+-- g.loaded_node_provider   = 0
+-- g.loaded_ruby_provider   = 0
+g.loaded_perl_provider   = 0
+g.loaded_python_provider = 0
 
-   set smartindent
-   set tabstop=4
-   set expandtab
+-- Plugins options {{{
 
-]], '')
--- require('plugins')
+-- }}}
 
