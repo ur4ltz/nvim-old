4d6b5ba87cd40b765d845feae7f46bf6f0d30339
Andy Shevchenko <ur4ltz@gmail.com>
2020-12-30 09:58:30 +0200

    Change telescope config

--- lua/conf/telescope.lua
+++ lua/conf/telescope.lua
@@ -1,14 +1,30 @@
-require('telescope').setup{
-    defaults = {
-        vimgrep_arguments = {
-            'rg',
-            '--color=never',
-            '--no-heading',
-            '--with-filename',
-            '--line-number',
-            '--column',
-            '--smart-case'
+M = {}
+
+function M.setup()
+    local opts = {noremap = true, silent = true}
+    vim.api.nvim_set_keymap('n', '<Space>h', "<cmd>lua require('telescope.builtin').help_tags{}<CR>", opts)
+end
+
+function M.config()
+    local telescope = require('telescope')
+    telescope.setup {
+        defaults = {
+            layout_strategy = 'flex',
+            layout_default = {
+                horizontal = {
+                    width_padding = 0.1,
+                    height_padding = 0.1,
+                    preview_width = 0.6,
+                },
+                vertical = {
+                    width_padding = 0.05,
+                    height_padding = 1,
+                    preview_width = 0.5,
+                },
+            },
+            preview_cutoff = 100,
         },
-        color_devicons = true
     }
-}
+end
+
+return M
