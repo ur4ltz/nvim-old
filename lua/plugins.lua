8f31fe7e294d7b1b5e99802fa8dc620eccc13db7
Andy Shevchenko <ur4ltz@gmail.com>
2021-01-18 19:53:23 +0200

    lualine: reformat code, added signify.

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -26,22 +26,22 @@ require('packer').startup(function()
             lualine.theme = 'powerline'
             lualine.separator = '|'
             lualine.sections = {
-                lualine_a = { 'mode' },
-                lualine_b = { 'branch' },
-                lualine_c = { 'filename' },
-                lualine_x = { 'encoding', 'fileformat', 'filetype' },
-                lualine_y = { 'progress' },
-                lualine_z = { 'location'  },
+                lualine_a = {'mode'},
+                lualine_b = {'branch', 'signify'},
+                lualine_c = {'filename'},
+                lualine_x = {'encoding', 'fileformat', 'filetype'},
+                lualine_y = {'progress'},
+                lualine_z = {'location'},
             }
             lualine.inactive_sections = {
-                lualine_a = {  },
-                lualine_b = {  },
-                lualine_c = { 'filename' },
-                lualine_x = { 'location' },
-                lualine_y = {  },
-                lualine_z = {   }
+                lualine_a = {},
+                lualine_b = {},
+                lualine_c = {'filename'},
+                lualine_x = {'location'},
+                lualine_y = {},
+                lualine_z = {}
             }
-            lualine.extensions = { 'fzf' }
+            lualine.extensions = {'fzf'}
             lualine.status()
         end
     }
