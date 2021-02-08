6648a7bff3f8223b1d700f44b7a8c1072f430675
Andy Shevchenko <ur4ltz@gmail.com>
2021-02-08 17:42:19 +0200

    Updated indent-guides.

--- lua/conf/indent-guides.lua
+++ lua/conf/indent-guides.lua
@@ -1,13 +1,15 @@
 -- vim: ts=4 sw=4 et:
 
-require('indent_guides').options = {
+require('indent_guides').setup {
     indent_levels        = 30,
     indent_start_level   = 1,
     indent_space_guides  = true,
     indent_tab_guides    = true,
     indent_pretty_guides = false,
     indent_soft_pattern  = '\\s',
-    exclude_filetypes    = {'help'}
+    exclude_filetypes    = {'NvimTree', 'packer', 'help'},
+    even_colors          = {fg = '#2E323A',bg = '#34383F'},
+    odd_colors           = {fg = '#34383F',bg = '#2E323A'}
 
 }
 
